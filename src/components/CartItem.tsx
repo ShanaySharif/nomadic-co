444827c42f9170e848a56e2d74006ce638bb2aec
ShanaySharif <Shanay.uw17@gmail.com>
2023-09-29 07:08:17 -0400

    add .env

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,19 +1,46 @@
-import { useShoppingCart } from "../context/ShoppingCartContext"
+import { useShoppingCart, } from "../context/ShoppingCartContext"
+import { Button, Stack } from "react-bootstrap"
+import { formatCurrency } from "../utilities/formatCurrency"
 import coffeeItems from "../data/items.json"
 
+
 type CartItemProps = {
-    id: number
-    quantity: number
-
+  id: number
+  quantity: number
 }
-export function CartItem ({ id, quantity }: CartItemProps) {
 
-    const { removeFromCart } = useShoppingCart ()
-    const item = coffeeItems
-    if ( item == null ) return null
+export function CartItem({ id, quantity }: CartItemProps) {
+  const { removeFromCart } = useShoppingCart()
+  const item = coffeeItems.find(i => i.id === id)
+  if (item == null) return null
 
-    return (
-    <Stack direction = "horizontal" gap={2}
-    
-    )
+  return (
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
+      <img
+        src={item.imgUrl}
+        style={{ width: "125px", height: "75px", objectFit: "cover" }}
+      />
+      <div className="me-auto">
+        <div>
+          {item.name}{" "}
+          {quantity > 1 && (
+            <span className="text-muted" style={{ fontSize: ".65rem" }}>
+              x{quantity}
+            </span>
+          )}
+        </div>
+        <div className="text-muted" style={{ fontSize: ".75rem" }}>
+          {formatCurrency(item.price)}
+        </div>
+      </div>
+      <div> {formatCurrency(item.price * quantity)}</div>
+      <Button
+        variant="outline-danger"
+        size="sm"
+        onClick={() => removeFromCart(item.id)}
+      >
+        &times;
+      </Button>
+    </Stack>
+  )
 }
--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,37 +1,36 @@
-import { Offcanvas } from "react-bootstrap";
-import { useShoppingCart } from "../context/ShoppingCartContext";
-// off canvas gives the slid effect to the cart bar
-
+import { Offcanvas, Stack } from "react-bootstrap"
+import { useShoppingCart } from "../context/ShoppingCartContext"
+import { formatCurrency } from "../utilities/formatCurrency"
+import { CartItem } from "./CartItem"
+import coffeeItems from "../data/items.json"
 
 type ShoppingCartProps = {
-    isOpen: boolean
+  isOpen: boolean
 }
 
-
-export function ShoppingCart({isOpen}): ShoppingCartProps){
-    const { closeCart } = useShoppingCart ()
-
-    return (
-        <Offcanvas show={isOpen} onHide={closeCart} placement="end">
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title>Cart</Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            <Stack gap={3}>
-              {cartItems.map(item => (
-                <CartItem key={item.id} {...item} />
-              ))}
-              <div className="ms-auto fw-bold fs-5">
-                Total{" "}
-                {formatCurrency(
-                  cartItems.reduce((total, cartItem) => {
-                    const item = coffeeItems.find(i => i.id === cartItem.id)
-                    return total + (item?.price || 0) * cartItem.quantity
-                  }, 0)
-                )}
-              </div>
-            </Stack>
-          </Offcanvas.Body>
-        </Offcanvas>
-      )
-    }
+export function ShoppingCart({ isOpen }: ShoppingCartProps) {
+  const { closeCart, cartItems } = useShoppingCart()
+  return (
+    <Offcanvas show={isOpen} onHide={closeCart} placement="end">
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>Cart</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Stack gap={3}>
+          {cartItems.map(item => (
+            <CartItem key={item.id} {...item} />
+          ))}
+          <div className="ms-auto fw-bold fs-5">
+            Total{" "}
+            {formatCurrency(
+              cartItems.reduce((total, cartItem) => {
+                const item = coffeeItems.find(i => i.id === cartItem.id)
+                return total + (item?.price || 0) * cartItem.quantity
+              }, 0)
+            )}
+          </div>
+        </Stack>
+      </Offcanvas.Body>
+    </Offcanvas>
+  )
+}
