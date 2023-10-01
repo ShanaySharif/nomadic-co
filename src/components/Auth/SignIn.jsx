7c4105dfc0b5d6ce564e14a3783522f97dd2c601
ShanaySharif <Shanay.uw17@gmail.com>
2023-10-01 10:40:19 -0400

    update firebase file

--- src/components/Auth/SignIn.jsx
+++ src/components/Auth/SignIn.jsx
@@ -1,13 +1,28 @@
+import { signInWithEmailAndPassword } from "firebase/auth"
 import React, { useState } from "react";
+import {auth} from "../firebase"
 
 
 
 const SignIn = () => {
+
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('');
+
+    //creat func that will handel logic for when form is submitted 
+    const signIn = (e) =>  {
+        e.preventDefault(); //so formsubmit will not reload/wont lose state
+        signInWithEmailAndPassword (auth, email, password )
+        .then((userCredential) => {
+            console.log(userCredential);
+        })
+        .catch((error) =>  {
+            console.log(error);
+        });
+    };
     return (
         <div className="sign-in-countainer">
-            <form>
+            <form onSubmit={signIn}>
                 <h1> Log In</h1>
                 <input type="email" placeholder="Enter your email"
                     value={email}
@@ -17,6 +32,7 @@ const SignIn = () => {
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                 ></input>
+                <button type=" submit ">Log In</button>
             </form>
         </div>
     );
--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,17 +1,24 @@
-/* eslint-disable no-undef */
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
 
-
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID 
-}
+  apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_SENDER_ID,
+  appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID
+};
+
+
+
+// //initialize firebase
+// const app = initializeApp(firebaseConfig);
+// //initilize birebase authentication
+// export const auth = getAuth(app);
+
+// export { auth };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -19,4 +26,11 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 export { db, auth };
+
+
+
+
+
+
+
 
