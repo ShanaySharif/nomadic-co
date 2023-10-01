d860cd835cc80d3d9979ad53b9bad8e0b98a3e8f
ShanaySharif <Shanay.uw17@gmail.com>
2023-10-01 08:15:39 -0400

    attempting to add auth via firebase

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,18 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 
 const SignIn = () => {
+  const [email, setEmail] = useState ('')
+  const [password, setPassword]
     return (
         <div className="sign-in-countainer">
             <form>
                 <h1> Log In</h1>
                 <input type="email" placeholder="Enter your email"></input>
-
-
             </form>
-
-
-
         </div>
     )
 
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SignIn from "./SignIn"; // Adjust the import path to match your project structure
+
+ReactDOM.render(
+  <React.StrictMode>
+    <SignIn />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
