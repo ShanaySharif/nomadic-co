76b0b9c44094e330f33e17db4727742ffd0e794b
ShanaySharif <Shanay.uw17@gmail.com>
2023-10-02 16:57:07 -0400

    add styling to sign up

--- src/components/Auth/SignIn.jsx
+++ src/components/Auth/SignIn.jsx
@@ -1,6 +1,114 @@
+// import { signInWithEmailAndPassword } from "firebase/auth";
+// import React, { useState } from "react";
+// import { auth } from "../firebase";
+
+// const SignIn = ({ toggleForm }) => {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   const signIn = (e) => {
+//     e.preventDefault();
+//     signInWithEmailAndPassword(auth, email, password)
+//       .then((userCredential) => {
+//         console.log(userCredential);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   };
+
+//   return (
+//     <div className="sign-in-container">
+//       <form onSubmit={signIn}>
+//         <h1>Log In</h1>
+//         <input
+//           type="email"
+//           placeholder="Enter your email"
+//           value={email}
+//           onChange={(e) => setEmail(e.target.value)}
+//         />
+//         <input
+//           type="password"
+//           placeholder="Enter your password"
+//           value={password}
+//           onChange={(e) => setPassword(e.target.value)}
+//         />
+//         <button type="submit">Log In</button>
+//       </form>
+//       <p>
+//         Don't have an account?{" "}
+//         <button onClick={toggleForm}>Sign Up</button>
+//       </p>
+//     </div>
+//   );
+// };
+
+// export default SignIn;
+
+
 import { signInWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { auth } from "../firebase";
+import styled from "styled-components"; // Import styled-components
+
+const SignInContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const SignInForm = styled.form`
+  background-color: #fff;
+  border-radius: 8px;
+  padding: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  width: 300px;
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  margin-bottom: 20px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 10px;
+  margin: 10px 0;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+const SubmitButton = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: #0074cc;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #005aaa;
+  }
+`;
+
+const SignUpText = styled.p`
+  margin-top: 20px;
+`;
+
+const SignUpButton = styled.button`
+  background: none;
+  border: none;
+  color: #0074cc;
+  cursor: pointer;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 const SignIn = ({ toggleForm }) => {
   const [email, setEmail] = useState("");
@@ -18,28 +126,28 @@ const SignIn = ({ toggleForm }) => {
   };
 
   return (
-    <div className="sign-in-container">
-      <form onSubmit={signIn}>
-        <h1>Log In</h1>
-        <input
+    <SignInContainer>
+      <SignInForm onSubmit={signIn}>
+        <Title>Log In</Title>
+        <Input
           type="email"
           placeholder="Enter your email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-        <input
+        <Input
           type="password"
           placeholder="Enter your password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit">Log In</button>
-      </form>
-      <p>
+        <SubmitButton type="submit">Log In</SubmitButton>
+      </SignInForm>
+      <SignUpText>
         Don't have an account?{" "}
-        <button onClick={toggleForm}>Sign Up</button>
-      </p>
-    </div>
+        <SignUpButton onClick={toggleForm}>Sign Up</SignUpButton>
+      </SignUpText>
+    </SignInContainer>
   );
 };
 
