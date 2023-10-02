d52ac1d37e8d0dbd3a585dfce270c0b73a16f155
ShanaySharif <Shanay.uw17@gmail.com>
2023-10-02 12:47:36 -0400

    add authdetails and set up signin/signout allowing user to know when they're signed in and with what email they're signed in with

--- src/components/Auth/AuthDetails.jsx
+++ src/components/Auth/AuthDetails.jsx
@@ -1,7 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase";
 
-const AuthDetails = () {
+const AuthDetails = () => {
+    const [authUser, setAuthUser] = useState (null);
+    useEffect (() => {
+        const listen = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setAuthUser (user);
 
-}
+            } else {
+                setAuthUser(null);
+            }
+        })
+    })
+    return <div>{authUser ? <p>{'Signed In as ${authUser.email}'}</p> : <p>Signed Out</p>}</div>;
+
+};
 
 export default AuthDetails;
